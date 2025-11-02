604552683675fb4b3ce2f67981279bf36876bd73
LabzTheKnight <aaafolayan@gmail.com>
2025-11-02 16:15:24 +0100

    picture fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,27 @@
 require 'faker'
 require 'open-uri'
 
+# Fetch a remote image with browser-like headers and a single retry to reduce 403 hotlink errors
+# This does NOT fallback to local assets — it will raise on persistent failure so you can see the problem.
+def fetch_image(url)
+  ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0 Safari/537.36"
+  opts = { "User-Agent" => ua, "Accept" => "image/*" }
+  begin
+    host = URI.parse(url).host
+    opts["Referer"] = "https://#{host}" if host
+  rescue StandardError
+    # ignore parse errors
+  end
+
+  begin
+    URI.open(url, opts)
+  rescue OpenURI::HTTPError, Errno::ETIMEDOUT, SocketError => e
+    # one retry with a short sleep
+    sleep 1
+    URI.open(url, opts)
+  end
+end
+
 Meal.destroy_all
 User.destroy_all
 
@@ -29,9 +50,9 @@ eri = User.create(email: 'eri@gmail.com', password: '1234567', nickname: 'Eri')
 natalia = User.create(email: 'natalia@gmail.com', password: '1234567', nickname: 'Natalia')
 diego = User.create(email: 'diego@gmail.com', password: '1234567', nickname: 'Diego')
 
-file30 = URI.open("https://avatars.githubusercontent.com/u/77808978?v=4")
-file31 = URI.open("https://avatars.githubusercontent.com/u/108654212?v=4")
-file32 = URI.open("https://avatars.githubusercontent.com/u/108351320?v=4")
+file30 = fetch_image("https://avatars.githubusercontent.com/u/77808978?v=4")
+file31 = fetch_image("https://avatars.githubusercontent.com/u/108654212?v=4")
+file32 = fetch_image("https://avatars.githubusercontent.com/u/108351320?v=4")
 
 afolabi.photos.attach(io: file30, filename: 'afo', content_type: 'image/jpg')
 hai.photos.attach(io: file31, filename: 'hai', content_type: 'image/jpg')
@@ -56,9 +77,9 @@ tonkotsuramen = Meal.new(
   expiry: "12.09.2022",
   address: "Spichernstraße 8, 50672 Cologne"
 )
-file1 = URI.open("https://takumi.koeln/wp-content/uploads/2021/10/ramen-koeln-2-shio.jpg")
-file22 = URI.open("https://res.cloudinary.com/qhai/image/upload/v1662630383/production/87bc54_710f936f47774a0589e6a7ff481a9c2d_mv2_aprpvk.webp")
-file23 = URI.open("https://res.cloudinary.com/qhai/image/upload/v1662630383/production/ramen-takumi-koeln-4_efgody.jpg")
+file1 = fetch_image("https://takumi.koeln/wp-content/uploads/2021/10/ramen-koeln-2-shio.jpg")
+file22 = fetch_image("https://res.cloudinary.com/qhai/image/upload/v1662630383/production/87bc54_710f936f47774a0589e6a7ff481a9c2d_mv2_aprpvk.webp")
+file23 = fetch_image("https://res.cloudinary.com/qhai/image/upload/v1662630383/production/ramen-takumi-koeln-4_efgody.jpg")
 tonkotsuramen.photos.attach([{io: file1, filename: 'ramen1', content_type: 'image/jpg'}, {io: file22, filename: 'ramen22', content_type: 'image/jpg'}, {io: file23, filename: 'ramen23', content_type: 'image/jpg'}])
 tonkotsuramen.user = hai
 tonkotsuramen.save
@@ -75,7 +96,7 @@ beefshrooms = Meal.new(
   expiry: "14.09.2022",
   address: "Meister-Ekkehart-Straße 9, 50937 Cologne"
 )
-file2 = URI.open("https://images.unsplash.com/photo-1532550907401-a500c9a57435?w=800")
+file2 = fetch_image("https://images.unsplash.com/photo-1532550907401-a500c9a57435?w=800")
 beefshrooms.photos.attach(io: file2, filename: 'beefshrooms', content_type: 'image/jpg')
 beefshrooms.user = ronna
 beefshrooms.save
@@ -94,7 +115,7 @@ salchow = Meal.new(
   expiry: "15.09.2022",
   address: "Zülpicher Straße 203, 50937 Cologne"
 )
-file3 = URI.open("https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?w=800")
+file3 = fetch_image("https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?w=800")
 salchow.photos.attach(io: file3, filename: 'salchow', content_type: 'image/jpg')
 salchow.user = josephine
 salchow.save
@@ -112,7 +133,7 @@ creojam = Meal.new(
   expiry: "12.09.2022",
   address: "Lothringer Straße 22, 50677 Cologne"
 )
-file4 = URI.open("https://images.unsplash.com/photo-1588137378633-dea1336ce1e2?w=800")
+file4 = fetch_image("https://images.unsplash.com/photo-1588137378633-dea1336ce1e2?w=800")
 creojam.photos.attach(io: file4, filename: 'creojam', content_type: 'image/jpg')
 creojam.user = ruby
 creojam.save
@@ -129,9 +150,9 @@ sumrolls = Meal.new(
   expiry: "10.09.2022",
   address: "Maternusstraße 15, 50678 Cologne"
 )
-file5 = URI.open("https://cdn.statically.io/img/www.aheadofthyme.com/f=auto//wp-content/uploads/2021/07/vietnamese-summer-rolls-with-chicken-2.jpg")
-file40 = URI.open("https://cdn.tgdd.vn/Files/2021/08/28/1378488/cach-lam-goi-cuon-bo-xao-sa-thom-lung-ngon-mieng-dai-ca-nha-202201051121002357.jpg")
-file41 = URI.open("https://www.resipis.de/wp-content/uploads/2016/07/vietnamesische-sommerrollen-mit-garnelen-und-nuoc-cham-1.jpg")
+file5 = fetch_image("https://cdn.statically.io/img/www.aheadofthyme.com/f=auto//wp-content/uploads/2021/07/vietnamese-summer-rolls-with-chicken-2.jpg")
+file40 = fetch_image("https://cdn.tgdd.vn/Files/2021/08/28/1378488/cach-lam-goi-cuon-bo-xao-sa-thom-lung-ngon-mieng-dai-ca-nha-202201051121002357.jpg")
+file41 = fetch_image("https://www.resipis.de/wp-content/uploads/2016/07/vietnamesische-sommerrollen-mit-garnelen-und-nuoc-cham-1.jpg")
 sumrolls.photos.attach([{io: file5, filename: 'sumrolls', content_type: 'image/jpg'}, {io: file40, filename: 'sumrolls2', content_type: 'image/jpg'}, {io: file41, filename: 'sumrolls3', content_type: 'image/jpg'}])
 sumrolls.user = hai
 sumrolls.save
@@ -148,7 +169,7 @@ jollof = Meal.new(
   expiry: "12.09.2022",
   address: "Karl-Korn-Straße 10, 50678 Cologne"
 )
-file6 = URI.open("https://cheflolaskitchen.com/wp-content/uploads/2018/10/Jollof-Rice-55.jpg")
+file6 = fetch_image("https://cheflolaskitchen.com/wp-content/uploads/2018/10/Jollof-Rice-55.jpg")
 jollof.photos.attach(io: file6, filename: 'jollof', content_type: 'image/jpg')
 jollof.user = afolabi
 jollof.save
@@ -165,7 +186,7 @@ beefpaprikash = Meal.new(
   expiry: "12.09.2022",
   address: "Biberstraße 32, 50678 Cologne"
 )
-file7 = URI.open("https://images.unsplash.com/photo-1546833999-b9f581a1996d?w=800")
+file7 = fetch_image("https://images.unsplash.com/photo-1546833999-b9f581a1996d?w=800")
 beefpaprikash.photos.attach(io: file7, filename: 'beefpaprikash', content_type: 'image/jpg')
 beefpaprikash.user = jeff
 beefpaprikash.save
@@ -183,7 +204,7 @@ chickenriceskillet = Meal.new(
   expiry: "12.09.2022",
   address: "Landsbergstraße 26, 50678 Cologne"
 )
-file8 = URI.open("https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=800")
+file8 = fetch_image("https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=800")
 chickenriceskillet.photos.attach(io: file8, filename: 'chickenriceskillet', content_type: 'image/jpg')
 chickenriceskillet.user = jeff
 chickenriceskillet.save
@@ -201,7 +222,7 @@ beanbeefchili = Meal.new(
   expiry: "14.09.2022",
   address: "Löwengasse 9, 50676 Cologne"
 )
-file9 = URI.open("https://images.unsplash.com/photo-1556909114-f6e7ad7d3136?w=800")
+file9 = fetch_image("https://images.unsplash.com/photo-1556909114-f6e7ad7d3136?w=800")
 beanbeefchili.photos.attach(io: file9, filename: 'beanbeefchili', content_type: 'image/jpg')
 beanbeefchili.user = damon
 beanbeefchili.save
@@ -218,7 +239,7 @@ haystraw = Meal.new(
   expiry: "14.09.2022",
   address: "Krummer Büchel 2, 50676 Cologne"
 )
-file10 = URI.open("https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?w=800")
+file10 = fetch_image("https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?w=800")
 haystraw.photos.attach(io: file10, filename: 'haystraw', content_type: 'image/jpg')
 haystraw.user = sabrina
 haystraw.save
@@ -235,8 +256,8 @@ beansoup = Meal.new(
   expiry: "10.09.2022",
   address: "Wöhlerstraße 35, 50823 Cologne"
 )
-# Use a local fallback image to avoid remote fetch failures (some hosts return 403)
-file11 = File.open(Rails.root.join('app', 'assets', 'images', 'avatar_default.jpg'))
+# attach remote image for bean soup (no fallback)
+file11 = fetch_image("https://www.kuechengoetter.de/uploads/media/630x630/00/5760-gruener-bohneneintopf.jpg?v=1-0")
 beansoup.photos.attach(io: file11, filename: 'beansoup.jpg', content_type: 'image/jpg')
 beansoup.user = justus
 beansoup.save
@@ -254,9 +275,9 @@ frenchtoast = Meal.new(
   expiry: "10.09.2022",
   address: "Siebachstraße 105, 50733 Cologne"
 )
-file12 = URI.open("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-220823-hong-kong-french-toast-388-horizontal-rv-web-1662038739.jpg?crop=0.895xw:0.855xh;0.0646xw,0.128xh&resize=980:*")
-file23 = URI.open("https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B711%2C451%5D&w=1582&h=791&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F08%2F7016-french-toast-mfs-010.jpg")
-file24 = URI.open("https://www.simplyrecipes.com/thmb/1r6e-oOPSGBkxbxY13tcRE1AvA8=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-French-Toast-Lead-Shot-3b-c3a68a576a9548f5bd43cce3d2d7f4b7.jpg")
+file12 = fetch_image("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-220823-hong-kong-french-toast-388-horizontal-rv-web-1662038739.jpg?crop=0.895xw:0.855xh;0.0646xw,0.128xh&resize=980:*")
+file23 = fetch_image("https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B711%2C451%5D&w=1582&h=791&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F08%2F7016-french-toast-mfs-010.jpg")
+file24 = fetch_image("https://www.simplyrecipes.com/thmb/1r6e-oOPSGBkxbxY13tcRE1AvA8=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-French-Toast-Lead-Shot-3b-c3a68a576a9548f5bd43cce3d2d7f4b7.jpg")
 
 frenchtoast.photos.attach([{io: file12, filename: 'frenchtoast', content_type: 'image/jpg'}, {io: file23, filename: 'frenchtoast2', content_type: 'image/jpg'}, {io: file24, filename: 'frenchtoast3', content_type: 'image/jpg'}])
 frenchtoast.user = johnson
@@ -275,7 +296,7 @@ brusselssprout = Meal.new(
   expiry: "12.09.2022",
   address: "Mauenheimer Straße 20, 50733 Cologne"
 )
-file13 = URI.open("https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/1520902273-shot-1-56.jpg?crop=1.0xw:1xh;center,top&resize=980:*")
+file13 = fetch_image("https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/1520902273-shot-1-56.jpg?crop=1.0xw:1xh;center,top&resize=980:*")
 brusselssprout.photos.attach(io: file13, filename: 'brusselssprout', content_type: 'image/jpg')
 brusselssprout.user = eri
 brusselssprout.save
@@ -293,7 +314,7 @@ koreanfriedchicken = Meal.new(
   expiry: "12.09.2022",
   address: "Erzbergerplatz 16, 50733 Cologne"
 )
-file14 = URI.open("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/singleimagebug-korean-fried-chicken-1549305127.jpg?crop=1xw:0.9453781512605042xh;center,top&resize=980:*")
+file14 = fetch_image("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/singleimagebug-korean-fried-chicken-1549305127.jpg?crop=1xw:0.9453781512605042xh;center,top&resize=980:*")
 koreanfriedchicken.photos.attach(io: file14, filename: 'korean-fried-chicken', content_type: 'image/jpg')
 koreanfriedchicken.user = natalia
 koreanfriedchicken.save
@@ -312,7 +333,7 @@ beefb = Meal.new(
   expiry: "13.09.2022",
   address: "Appellhofplatz 16, 50667 Cologne"
 )
-file15 = URI.open("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190828-beef-bourguignon-0154-portrait-pf-1568132096.jpg?crop=1xw:0.9994736842105263xh;center,top&resize=768:*&keepGifs=1")
+file15 = fetch_image("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190828-beef-bourguignon-0154-portrait-pf-1568132096.jpg?crop=1xw:0.9994736842105263xh;center,top&resize=768:*&keepGifs=1")
 beefb.photos.attach(io: file15, filename: 'beef-b', content_type: 'image/jpg')
 beefb.user = william
 beefb.save
@@ -330,7 +351,7 @@ creamypasta = Meal.new(
   expiry: "13.09.2022",
   address: "Rheingasse 15, 50676 Cologne"
 )
-file16 = URI.open("https://cdn.loveandlemons.com/wp-content/uploads/2019/12/vegan-recipes.jpg")
+file16 = fetch_image("https://cdn.loveandlemons.com/wp-content/uploads/2019/12/vegan-recipes.jpg")
 creamypasta.photos.attach(io: file16, filename: 'creamy-pasta', content_type: 'image/jpg')
 creamypasta.user = diego
 creamypasta.save
@@ -347,7 +368,7 @@ zucchinifritters = Meal.new(
   expiry: "11.09.2022",
   address: "Leonhard-Tietz-Straße 8, 50676 Cologne"
 )
-file17 = URI.open("https://www.thespruceeats.com/thmb/LzlC4zHWABM91kFC76Zl6RAZqkk=/3244x2433/smart/filters:no_upscale()/zucchinifritters-56a5b26b5f9b58b7d0de04f2.jpg")
+file17 = fetch_image("https://www.thespruceeats.com/thmb/LzlC4zHWABM91kFC76Zl6RAZqkk=/3244x2433/smart/filters:no_upscale()/zucchinifritters-56a5b26b5f9b58b7d0de04f2.jpg")
 zucchinifritters.photos.attach(io: file17, filename: 'zucchini-fritters', content_type: 'image/jpg')
 zucchinifritters.user = diego
 zucchinifritters.save
@@ -365,7 +386,7 @@ caribbeantofu = Meal.new(
   expiry: "12.09.2022",
   address: "Große Telegraphenstraße 34, 50676 Cologne"
 )
-file18 = URI.open("https://veganuary.com/wp-content/uploads/2016/09/Caribbean-Tofu-2.jpg")
+file18 = fetch_image("https://veganuary.com/wp-content/uploads/2016/09/Caribbean-Tofu-2.jpg")
 caribbeantofu.photos.attach(io: file18, filename: 'caribbean-tofu', content_type: 'image/jpg')
 caribbeantofu.user = hai
 caribbeantofu.save
@@ -383,7 +404,7 @@ vegancobbler = Meal.new(
   expiry: "14.09.2022",
   address: "Huhnsgasse 34, 50676 Cologne"
 )
-file19 = URI.open("https://rainbowplantlife.com/wp-content/uploads/2020/11/peachblueberrrycobbler287of1029.jpg")
+file19 = fetch_image("https://rainbowplantlife.com/wp-content/uploads/2020/11/peachblueberrrycobbler287of1029.jpg")
 vegancobbler.photos.attach(io: file19, filename: 'vegan-cobbler', content_type: 'image/jpg')
 vegancobbler.user = afolabi
 vegancobbler.save
@@ -401,7 +422,7 @@ tuscanchicken = Meal.new(
   expiry: "14.09.2022",
   address: "Lothringer Straße 35, 50677 Cologne"
 )
-file20 = URI.open("https://img.taste.com.au/1KNqPo-z/w720-h480-cfill-q80/taste/2021/10/tuscan-chicken-rissoles-recipe-174147-2.jpg")
+file20 = fetch_image("https://img.taste.com.au/1KNqPo-z/w720-h480-cfill-q80/taste/2021/10/tuscan-chicken-rissoles-recipe-174147-2.jpg")
 tuscanchicken.photos.attach(io: file20, filename: 'tuscan-chicken', content_type: 'image/jpg')
 tuscanchicken.user = axel
 tuscanchicken.save
@@ -419,7 +440,7 @@ choclatecake = Meal.new(
   expiry: "15.09.2022",
   address: "Paul-Schallück-Straße 7, 50939 Cologne"
 )
-file21 = URI.open("https://sallysbakingaddiction.com/wp-content/uploads/2013/04/triple-chocolate-cake-4.jpg")
+file21 = fetch_image("https://sallysbakingaddiction.com/wp-content/uploads/2013/04/triple-chocolate-cake-4.jpg")
 choclatecake.photos.attach(io: file21, filename: 'choclate-cake', content_type: 'image/jpg')
 choclatecake.user = axel
 choclatecake.save
